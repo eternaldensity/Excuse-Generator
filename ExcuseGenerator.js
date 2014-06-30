5cb348031f31e59c75c2f460a99f9e16ed9b4a43
Hotdog Vendor <ed@chirpingmustard.com>
2014-06-30 15:38:47 +1000

    New files with a bunch of options added.

---
+++ ExcuseGenerator.js
@@ -0,0 +1,398 @@
+/*
+ * (Not Really Armsmaster's) Random Excuse Generator
+ * Generates excuses. Which are random.
+ *
+ * Coded by EldritchPangolin (who's unfortunately not Armsmaster)
+ *
+ */
+
+"use strict";
+
+function factoryFun(prototype, constructor) {
+    return function() {
+        var instance = Object.create(prototype);
+        constructor.apply(instance, arguments);
+        return instance;
+    }
+}
+
+// generator namespace
+var G = function() {
+    var G = {};
+
+    var quotePrototype = {}
+    quotePrototype.generate = function() {
+        return this.str;
+    }
+    G.Quote = factoryFun(quotePrototype, function(str) {
+        this.str = str;
+    })
+
+    // wrappers
+
+    var postprocessPrototype = {};
+    postprocessPrototype.generate = function() {
+        return this.fun(this.wrapped.generate());
+    }
+    G.Postprocess = factoryFun(postprocessPrototype, function(wrapped, fun) {
+        this.wrapped = readGen(wrapped);
+        this.fun = fun;
+    })
+
+    var rollUntilPrototype = {};
+    rollUntilPrototype.generate = function() {
+        var result;
+        while (!this.fun(result = this.wrapped.generate())) { }
+        return result;
+    }
+    G.RollUntil = factoryFun(rollUntilPrototype, function(wrapped, fun) {
+        this.wrapped = readGen(wrapped);
+        this.fun = fun;
+    })
+
+    var repPrototype = {};
+    repPrototype.generate = function() {
+        var range = this.max - this.min + 1;
+        var repeats = this.min + Math.floor(Math.random() * range);
+        var result = [];
+        for (var i = 0; i < repeats; i++) {
+            result.push(this.wrapped.generate());
+        }
+        return result.join(' ');
+    }
+    G.Rep = factoryFun(repPrototype, function(wrapped, min, max) {
+        this.wrapped = readGen(wrapped);
+        this.min = min;
+        this.max = max;
+    })
+
+    G.Opt = function(wrapped) { return G.Rep(wrapped, 0, 1); }
+
+    // for escaping splicing in nested alternates
+    G.Id = function(wrapped) { return G.Rep(wrapped, 1, 1); }
+
+    // combinators
+
+    var seqPrototype = {}
+    seqPrototype.generate = function() {
+        return this.seq.map(function (g) { return g.generate(); }).join(" ");
+    }
+    seqPrototype.and = function(gen) {
+        this.seq.push(readGen(gen));
+    }
+    G.Seq = factoryFun(seqPrototype, function(seq) {
+        this.seq = seq.map(readGen);
+    })
+
+    var altPrototype = {};
+    altPrototype.or = function(alt, prob) {
+        if (typeof prob == 'number') {
+            this.nonUniform = true;
+        }
+        alt = readGen(alt);
+        if (altPrototype.isPrototypeOf(alt)) { // splice
+            Array.prototype.push.apply(this.alts, alt.alts);
+            Array.prototype.push.apply(this.probs, alt.probs);
+        } else {
+            this.alts.push(alt);
+            this.probs.push(prob);
+        }
+    }
+    altPrototype.normalizeDist = function() {
+        var undefs = [], defs = [];
+        var i;
+        var totalDefined = 0;
+        for (i = 0; i < this.probs.length; ++i) {
+            if (typeof this.probs[i] == 'number') {
+                totalDefined += this.probs[i];
+                defs.push(i);
+            } else {
+                undefs.push(i);
+            }
+        }
+        if (totalDefined > 1.0) {
+            for (i = 0; i < defs.length; ++i) {
+                this.probs[defs[i]] /= totalDefined;
+            }
+            for (i = 0; i < undefs.length; ++i) {
+                this.probs[undefs[i]] = 0;
+            }
+        } else {
+            var defaultProb = (1.0 - totalDefined) / undefs.length;
+            for (i = 0; i < undefs.length; ++i) {
+                this.probs[undefs[i]] = defaultProb;
+            }
+        }
+        this.normalizedDist = true;
+    }
+    altPrototype.generate = function() {
+        var chosen, roll, partialSum;
+        if (this.nonUniform) {
+            if (!this.normalizedDist) {
+                this.normalizeDist();
+            }
+            roll = Math.random();
+            chosen = 0;
+            partialSum = this.probs[0];
+            for (chosen = 0, partialSum = this.probs[0];
+                 partialSum < roll && chosen < this.alts.length - 1;
+                 partialSum += this.probs[++chosen]) { }
+        } else {
+            chosen = Math.floor(Math.random() * this.alts.length);
+        }
+        return this.alts[chosen].generate();
+    }
+    G.Alt = factoryFun(altPrototype, function(alts) {
+        this.alts = [];
+        this.nonUniform = false;
+        this.normalizedDist = false;
+        this.probs = [];
+        if (!alts) {
+            alts = [];
+        }
+        for (var i = 0; i < alts.length; ++i) {
+            this.or(alts[i]);
+        }
+    })
+
+    function readGen(gen) {
+        if (typeof gen == 'string') {
+            return G.Quote(gen);
+        } else if (Array.isArray(gen)) {
+            return G.Seq(gen);
+        } else {
+            return gen;
+        } 
+    }
+
+    return G;
+}()
+
+var excuseGenerator = function() {
+    var g;
+
+    var Hero = G.Alt(["Armsmaster", "Alexandria", "Eidolon", "Legend",
+            "Chevalier", "Miss Militia", "Mouse Protector", "Myrddin", "Chubster", /*"Dauntless" NOPE not him */ "Narwhal"]);
+    var Endbringer = G.Alt(["Simurgh", "Ziz", "Leviathan", "Behemoth",
+            "Hadhayosh"]);
+    var Professionals = G.Alt(["accountants", "auto mechanics", "clowns",
+            "pro wrestlers", "divorce lawyers", "barbers", "nightclub bouncers"]);
+    var Gang = G.Alt(["the Empire 88",
+                      "the Azn Bad Boys",
+                      "the Merchants",
+                      "a mysterious unnaturally competent woman in a suit",
+                      "the Slaugherhouse 9",
+                      "some meddling teenagers in a van",
+                      "the local chapter of the Hell's Angels",
+                      "the Girl Scout's cookie-selling brigade",
+			"the Leauge of Door to Door Salesmen"]);
+    var LocationSpec = G.Alt(["in the nearby park",
+                              "at the beach",
+                              "at Fugly Bob's",
+                              "in the Fortress Construction's board meeting room",
+                              "at the local retirement home",
+                              "in my neighborhood",
+			"in my mailbox",
+			"on my roof",
+			"in the rusted hulk of a ship",
+			"in the general post office",
+			"inside Arcadia High",
+			"in the Winslow High girls' locker room"]);
+    var Thingy = G.Alt(["wristwatch", "underwear", "necktie", "lunchbox",
+            "wallet", "cellphone", "baseball cap", "monocle", "waffle",
+            "buzzsaw", "saucepan", "HDMI cable", "sock", "toaster oven",
+	"muffin", "t-shirt", "belt and suspenders", "vase", "sippy cup", "beer coaster"]);
+    var WeirdThingyTrait = G.Alt(["radioactive", "thermonuclear", "ballistic",
+            "tactical", "AI-controlled", "antimatter", "monomolecular", "futuristic", "alternate", "metallic", "non-stick", "autotuned", "shrinking"]);
+    var WackyActivityProgressive = G.Alt(["mauling random passers-by",
+            ["setting up a muffin stall", LocationSpec],
+            ["spraypainting suggestive graffiti", LocationSpec],
+            ["writing sappy love notes to", Hero],
+            ["setting up an online dating profile for", Endbringer],
+            "miming the aristocrats joke",
+            "tracking mud all over the office",
+            "howling majestically to the moon",
+		"twerking", "conga dancing"]);
+
+    var Critter = G.Alt(["kitten", "squirrel", "puppy", "hedgehog", "pony",
+                         "antelope", "sugar ant", "black widow spider"]);
+    var WeirdCritterTrait = G.Alt(["pink", "sapient", "flying", "tentacled",
+            "insectoid", "gargantuan", "glittery", "long-toothed", "slimy", "skiny", "eldrich"]);
+    var Substance = G.Alt(["ice cream", "candy", "cocaine", "psychic energy",
+            "liquid nitrogen", "radioactive sludge", "jello", "clay","solidified air","orphans' tears"
+            ["liquefied", Critter, "flesh"]]);
+    var ThirdParty = G.Alt([["my pet", Critter], "my son", "my daughter", "my evil twin", "your mom", "my red-headed stepchild"]);
+    var PersonCrimePast = G.Alt(["pranked by",
+            "asked on a date by a member of", "tickled by", "glared at by",
+	 "winked at by", "ignored by", "recruited by", "teleported by", "sent to the future by"]);
+    var PropertyCrimePast = G.Alt(["stolen", "defaced", "ruined with spilled lemonade",
+            "vandalised", "spray painted", "replaced with a mirror image", "sent to Earth Aleph", "hidden in the Boat Graveyard", ["fed to",ThirdParty]]);
+    var ValueModifier = G.Alt(["vintage", "antique", "priceless",
+            "limited edition", "autographed", "heirloom"]);
+    var QualityModifier = G.Alt(["tinkertech", "handcrafted", "masterwork",
+            "customized", "dwarven", "restored", "recycled"]);
+    var Theme = G.Postprocess(Hero, function (s) { return s + "-themed"; });
+
+    // ModdedThingy
+    g = G.Seq([G.Opt(ValueModifier), G.Opt(QualityModifier),
+               G.Opt(Theme), Thingy]);
+    var ModdedThingy = G.Postprocess(g, function(s) {
+        if (/doll|action figure|plushie/.test(s)) {
+            // no redundantly themed merchandise
+            s = s.replace(/([A-Z][^\s]+\s*)*themed\s*/, "");
+        }
+        return s;
+    })
+
+    // TriggeringEntity
+    g = G.Alt(["I{!OWNER_POSSESSIVE=my}{!HAVE_PLACEHOLDER=have}",
+               ["my pet{!OWNER_POSSESSIVE=its}{!HAVE_PLACEHOLDER=has}", 
+                Critter],
+               "my daughter{!HAVE_PLACEHOLDER=has}{!OWNER_POSSESSIVE=her}",
+               "my son{!HAVE_PLACEHOLDER=has}{!OWNER_POSSESSIVE=his}"]); 
+    var TriggeringEntity = g;
+
+    var FunnyBodyPart = G.Alt(["ears", "toes", "eyes", "navel", "nostrils",
+            "armpits", "kneecaps", "elbows", "ponytail", "back hair"]);
+
+    // Power
+    g = G.Alt();
+    g.or("of acute alliteration{!CLASS_PLACEHOLDER=thinker}");
+    g.or(["{!CLASS_PLACEHOLDER=thinker}of", Critter, "migratory pattern",
+          "prediction"]);
+    g.or(["{!CLASS_PLACEHOLDER=blaster}of slaughtering firstborn",
+            G.Alt(["sons", "daughters"])]);
+    g.or(["{!CLASS_PLACEHOLDER=blaster}to shoot", Substance, "out of", 
+            "{OWNER_POSSESSIVE}", FunnyBodyPart]);
+    g.or(["{!CLASS_PLACEHOLDER=mover}of extreme", G.Alt(["flamenco", "disco",
+                "tango", "samba"]), 
+          "dancing"]);
+    g.or(["{!CLASS_PLACEHOLDER=tinker}of", WeirdThingyTrait, Thingy,
+            "construction"]);
+    g.or(["{!CLASS_PLACEHOLDER=tinker}of", WeirdCritterTrait, Critter,
+            "cloning"]);
+    g.or("{!CLASS_PLACEHOLDER=shaker}to conjure appropriate theme music");
+    function removeArticleWhenUncountable(s) {
+        if (/underwear/.test(s)) {
+            s = s.replace(" an ", " ");
+        }
+        return s;
+    }
+    g.or(G.Postprocess(["{!CLASS_PLACEHOLDER=master}to project an",
+                            "indestructible animate",
+                            WeirdThingyTrait, Thingy], 
+                       removeArticleWhenUncountable));
+    g.or(["{!CLASS_PLACEHOLDER=master}to create golems made of", Substance]);
+    g.or(["{!CLASS_PLACEHOLDER=breaker}to turn into a living mass of",
+            Substance]);
+    g.or(["{!CLASS_PLACEHOLDER=changer}to grow", G.Alt(["tentacles", "teeth",
+                "antennae", "eyestalks", "an epic beard", 
+                [Substance, "spikes"]]), 
+          "out of {OWNER_POSSESSIVE}", FunnyBodyPart]);
+    function noRedundantGargantuanness(s) {
+        return !/gargantuan/.test(s);
+    }
+    g.or(G.RollUntil(["{!CLASS_PLACEHOLDER=changer}to turn into a giant",
+                      WeirdCritterTrait, Critter], noRedundantGargantuanness));
+    g.or(["{!CLASS_PLACEHOLDER=stranger}to become invisible to",
+          G.Alt(["redheaded", "bald", "mohawk sporting", "mullet bearing"]),
+          Professionals, "wearing", G.Alt(["yarmulkas", "top hats", 
+                                           "flip-flops", "togas"])]);
+    var Power = g;
+
+    var SomeHero = G.Alt([Hero, ["that new hero with the power", Power]]);
+
+    // CitizenExcuse
+    g = G.Alt();
+    g.or(["hand over a", G.Rep(WeirdCritterTrait, 0, 2), "tinkertech", Critter,
+          "that I found", WackyActivityProgressive]);
+    g.or(["hand over a tinkertech", G.Rep(WeirdThingyTrait,0, 2), Thingy,
+          "that I found", LocationSpec]);
+    function noGangInfighting(s) {
+        var matches = s.match(/{B}([^{]*){AND}and(.*)/);
+        return matches[1].trim() != matches[2].trim();
+    }
+    g.or(G.RollUntil(["report a gang fight between{B}", Gang, "{AND}and", 
+                      Gang], noGangInfighting)); 
+    function noMembersOfContessa(s) {
+        return !/woman in a suit/.test(s);
+    }
+    g.or(G.RollUntil(["report superpowered members of", Gang, 
+                      "loitering", LocationSpec], noMembersOfContessa));
+    g.or(G.Postprocess(["report that", TriggeringEntity, 
+                            "{HAVE_PLACEHOLDER} triggered with the",
+                            "{CLASS_PLACEHOLDER} type power", Power],
+                       tagCopyFun('HAVE_PLACEHOLDER', 'CLASS_PLACEHOLDER',
+                                  'OWNER_POSSESSIVE')));
+    g.or(["report that", Endbringer, "has been sighted", LocationSpec]);
+    var CitizenExcuse = g;
+
+    // ReporterReason
+    g = G.Alt();
+    g.or(["discuss the recent victory of", SomeHero, "against", Gang]);
+    g.or(["interview", SomeHero, "about the recent spate of", Critter,
+         "abductions"]);
+    g.or(["interview", SomeHero, "about the recent wave of hate crime against",
+          Professionals]);
+    g.or(["interview", SomeHero, "about the recent outbreak of", Thingy, 
+          "theft"]);
+    var ReporterReason = g;
+
+    // VictimComplaint
+    g = G.Alt();
+    g.or(["complain that my", ModdedThingy, "has been", PropertyCrimePast, 
+          "by", Gang]);
+    function noMembersOfContessa2(s) {
+        return !/member of/.test(s) || !/woman in a suit/.test(s);
+    }
+    g.or(G.RollUntil(["complain that", ThirdParty, "has been", PersonCrimePast,
+                Gang], noMembersOfContessa2));
+    g.or(["complain that", ThirdParty, "has been hypnotised by", Gang, "into",
+          WackyActivityProgressive]);
+    g.or(["reclaim my stolen", ModdedThingy]);
+    var VictimComplaint = g;
+
+    var WrittenWork = G.Alt(["a cookbook", "a propaganda pamphlet",
+            "a philosophical treatise", "a bildungsroman", "a romance novel",
+            "a dendrology textbook", "a bestselling self-help series", "a galactic guide for hitchhikers", "the new edition of Parahumanology for Dummies", "an urban fantasy", "a poem",
+["the unofficial reveal-all biography of",Hero]]);
+
+    var Top = G.Alt();
+    Top.or(["I'm a concerned citizen. I'm here to", CitizenExcuse]);
+    Top.or(["I'm a victim of crime. I'm here to", VictimComplaint]);
+    Top.or(["I'm a marketing consultant. I'm here to discuss the design of",
+            "the new", Theme, Thingy], 0.08);
+    Top.or(["I'm a reporter. I'm here to", ReporterReason], 0.1);
+    Top.or(["I'm a writer. I'm here to ask some questions about", SomeHero,
+            "as research for", WrittenWork, "I'm writing"], 0.05);
+    return G.Postprocess(G.Postprocess(Top, removeTags),
+                         squashSpacesAddPeriod);
+
+    function removeTags(s) {
+        return s.replace(/{[^}]*}/g, "");
+    }
+
+    function squashSpacesAddPeriod(s) {
+        return s.replace(/\s+/, " ")+".";
+    }
+
+    function tagCopyFun() {
+        var tags = arguments;
+        var copier = function(s) {
+            var re;
+            for (var i = 0; i < tags.length; ++i) {
+                re = RegExp("{!" + tags[i] + "=([^}]*)}");
+                if (re.test(s)) {
+                    s = s.replace(RegExp("{"+tags[i]+"}"), s.match(re)[1]);
+                }
+            }
+            return s;
+        }
+        return copier;
+    }
+}()
+
+function newExcuse() {
+    var excuse = excuseGenerator.generate();
+    document.getElementById("excuse").textContent = excuse;
+}
